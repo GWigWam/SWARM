5d2893cd90090aa86a47e9d0f647f8b353655d0b
GWigWam <welwisam@gmail.com>
2019-10-12 19:19:43 +0200

    Draw stuff w/ pixi.js

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log('Hi from index.ts!');
+import * as PIXI from 'pixi.js';
+
+const app = new PIXI.Application({ antialias: true });
+document.body.appendChild(app.view);
+
+const graphics = new PIXI.Graphics();
+
+// draw a shape
+graphics.beginFill(0xFF3300);
+graphics.lineStyle(4, 0xffd900, 1);
+graphics.moveTo(50, 350);
+graphics.lineTo(250, 350);
+graphics.lineTo(100, 400);
+graphics.lineTo(50, 350);
+graphics.closePath();
+graphics.endFill();
+
+// draw polygon
+const path = [600, 370, 700, 460, 780, 420, 730, 570, 590, 520];
+
+graphics.lineStyle(0);
+graphics.beginFill(0x3500FA, 1);
+graphics.drawPolygon(path);
+graphics.endFill();
+
+app.stage.addChild(graphics);
