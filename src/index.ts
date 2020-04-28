966fc48fe14555cde640da35f3084232f5ab3e91
GWigWam <welwisam@gmail.com>
2020-04-28 23:33:21 +0200

    Init PIXI on window.onload for consistent viewsize

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,29 @@ import * as PIXI from 'pixi.js';
 import { Graphics } from './pixiExtensions';
 import World from './world';
 
-const app = new PIXI.Application({
-    antialias: true,
-    transparent: false,
-    width: document.body.clientWidth - 30,
-    height: document.body.clientHeight - 30,
-    backgroundColor: 0xE0E0FF
-});
-document.body.appendChild(app.view);
-
-const graphics = new Graphics();
-app.stage.addChild(graphics);
-
-const world = new World(app.view.width, app.view.height);
-
-app.ticker.add(() => {
-    const elapsedSec = app.ticker.elapsedMS / 1000;    
-    world.update(elapsedSec);
+window.onload = () => {
+    const elem = document.getElementById('display')!;    
+    const app = new PIXI.Application({
+        antialias: true,
+        transparent: false,
+        width: elem.clientWidth,
+        height: elem.clientHeight,
+        backgroundColor: 0xE0E0FF
+    });
+    elem.appendChild(app.view);
     
-    graphics.clear();
-    for(let g of world.getGraphics()) {
-        graphics.drawGraphic(g);
-    }
-});
+    const graphics = new Graphics();
+    app.stage.addChild(graphics);
+    
+    const world = new World(app.view.width, app.view.height);
+    
+    app.ticker.add(() => {
+        const elapsedSec = app.ticker.elapsedMS / 1000;    
+        world.update(elapsedSec);
+        
+        graphics.clear();
+        for(let g of world.getGraphics()) {
+            graphics.drawGraphic(g);
+        }
+    });
+};
